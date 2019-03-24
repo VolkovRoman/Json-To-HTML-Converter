2b59f022ef23da3eb190e5dd35a317bebc6a32df
VolkovRoman <romawolf@yandex.ru>
2019-03-24 15:18:05 +0300

    2nd task

--- converter.py
+++ converter.py
@@ -8,10 +8,8 @@ json_data = json.load(file_open)
 def json_to_html():
     dict_tags = []  # Create an empty dictionary
     for tag in json_data:  # Appending to dictionary formatted data from json file
-        if tag['title']:
-            dict_tags.append('<h1>' + str(tag["title"]) + '</h1>')
-        if tag['body']:
-            dict_tags.append('<p>' + str(tag["body"]) + '</p>')
+        for keys in tag.keys(): # Adding another 'for', that allows to use this converter with any tags
+            dict_tags.append('<'+keys+'>' + tag[keys] + '</'+keys+'>')
 
     output_data = (''.join(dict_tags))  # Merging formatted items into a string
 
--- test_converter.py
+++ test_converter.py
@@ -5,7 +5,7 @@ import converter
 class ConverterTest(unittest.TestCase):
     def test_task(self):
         self.assertEqual(converter.json_to_html(),  # Here the function result is compared with the expected result.
-                         '<h1>Title #1</h1><p>Hello World 1!</p><h1>Title #2</h1><p>Hello World 2!</p>')
+                         '<h3>Title #1</h3><div>Hello World 1!</div>')
 
 
 if __name__ == '__main__':
