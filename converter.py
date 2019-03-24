d13debe0aed33438275ff8d3c138f1f462b2a5d7
VolkovRoman <romawolf@yandex.ru>
2019-03-24 19:59:12 +0300

    3rd task

--- converter.py
+++ converter.py
@@ -8,8 +8,14 @@ json_data = json.load(file_open)
 def json_to_html():
     dict_tags = []  # Create an empty dictionary
     for tag in json_data:  # Appending to dictionary formatted data from json file
-        for keys in tag.keys(): # Adding another 'for', that allows to use this converter with any tags
+        dict_tags.append('<li>')  # Adding tag '<li>' in the start of the item
+        for keys in tag.keys():  # Adding another 'for', that allows to use this converter with any tags
             dict_tags.append('<'+keys+'>' + tag[keys] + '</'+keys+'>')
+        dict_tags.append('</li>')  # Adding tag '</li>'in the end of the item
+
+    # Adding tags '<ul>' and '</ul>' in the same way
+    dict_tags.insert(0, '<ul>')
+    dict_tags.insert(len(dict_tags), '</ul>')
 
     output_data = (''.join(dict_tags))  # Merging formatted items into a string
 
--- test_converter.py
+++ test_converter.py
@@ -5,7 +5,8 @@ import converter
 class ConverterTest(unittest.TestCase):
     def test_task(self):
         self.assertEqual(converter.json_to_html(),  # Here the function result is compared with the expected result.
-                         '<h3>Title #1</h3><div>Hello World 1!</div>')
+                         ('<ul><li><h3>Title #1</h3><div>Hello World 1!</div></li>'
+                         '<li><h3>Title #2</h3><div>Hello World 2!</div></li></ul>'))
 
 
 if __name__ == '__main__':
