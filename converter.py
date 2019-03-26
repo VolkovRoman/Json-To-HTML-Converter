179ad245f935bb4024ab620afcfdd11c8b962069
VolkovRoman <48675231+VolkovRoman@users.noreply.github.com>
2019-03-26 15:02:08 +0300

    4rth task

--- converter.py
+++ converter.py
@@ -4,15 +4,29 @@ import sys
 file_open = open(r'source/source.json', mode='r', encoding='Latin-1')
 json_data = json.load(file_open)
 
-
 def json_to_html():
     dict_tags = []  # Create an empty dictionary
-    for tag in json_data:  # Appending to dictionary formatted data from json file
+    inside_item=[]  # list for 4rth task
+    for element in json_data:  # Appending to dictionary formatted data from json file
         dict_tags.append('<li>')  # Adding tag '<li>' in the start of the item
-        for keys in tag.keys():  # Adding another 'for', that allows to use this converter with any tags
-            dict_tags.append('<'+keys+'>' + tag[keys] + '</'+keys+'>')
+        for keys in element.keys():  # Adding another 'for', that allows to use this converter with any tags
+          if isinstance(element[keys], list)==True: # In case our keys are list           
+            inside_item.append('<li>')  # Adding tag '<li>' in the start of the item
+            for num in range(0, len(element[keys])):
+              for el in element[keys][num].keys(): # 
+                inside_item.append('<'+str(el)+'>' + str(element[keys][0][el]) + '</'+str(el)+'>')
+              inside_item.append('</li>')  # Adding tag '</li>'in the end of the item
+              #  Wrap the item in top-level tags
+              inside_item.insert(0, '<ul>')
+              inside_item.insert(len(inside_item), '</ul>')
+              inside_item.insert(0, ('<'+str(keys)+'>'))
+              inside_item.insert(len(inside_item), ('/<'+str(keys)+'>'))   
+              inside_item_data = (''.join(inside_item))
+              dict_tags.append(inside_item_data)  # Adding created list into the main table     
+          else:
+            dict_tags.append('<'+str(keys)+'>' + str(element[keys]) + '</'+str(keys)+'>')
         dict_tags.append('</li>')  # Adding tag '</li>'in the end of the item
-
+    
     # Adding tags '<ul>' and '</ul>' in the same way
     dict_tags.insert(0, '<ul>')
     dict_tags.insert(len(dict_tags), '</ul>')
