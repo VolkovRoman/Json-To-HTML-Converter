f8af05bbdbb652784cc431a5f8253bd39e88c7ac
VolkovRoman <48675231+VolkovRoman@users.noreply.github.com>
2019-03-26 15:08:32 +0300

    4rth task

--- test_converter.py
+++ test_converter.py
@@ -5,8 +5,8 @@ import converter
 class ConverterTest(unittest.TestCase):
     def test_task(self):
         self.assertEqual(converter.json_to_html(),  # Here the function result is compared with the expected result.
-                         ('<ul><li><h3>Title #1</h3><div>Hello World 1!</div></li>'
-                         '<li><h3>Title #2</h3><div>Hello World 2!</div></li></ul>'))
+                         ('<ul><li><span>Title #1</span><content><ul><li><p>Example 1</p><header>header 1</header>'
+                          '</li></ul></content></li><li><div>div 1</div></li></ul>'))
 
 
 if __name__ == '__main__':
